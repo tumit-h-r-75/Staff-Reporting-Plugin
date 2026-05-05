63f6d102227f208cb300a0d8f0bc73030bc5276c
tumit-h-r-75 <tumithasan1@gmail.com>
2026-05-05 06:55:51 -0700

    make some chnages

--- uninstall.php
+++ uninstall.php
@@ -5,9 +5,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}bsr_reports");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}bsr_salary_settings");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}bsr_working_days");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}bsr_holidays");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}staff_reports");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}staff_salary_settings");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}staff_working_days");
 
 delete_option('bsr_version');
